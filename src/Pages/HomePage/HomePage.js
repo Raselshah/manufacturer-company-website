1a90098d719326e2bad1cde361dc4b9c6d2d771c
Md Rasel Shah <mdraselshah56@gmail.com>
2022-05-22 01:00:18 +0600

    extra section added

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,12 +1,16 @@
 import React from "react";
+import Footer from "../../Shared/Footer/Footer";
 import HomePageBanner from "./HomePageBanner/HomePageBanner";
 import OurProducts from "./OurProducts/OurProducts";
+import UpcommingProducts from "./UpcommingProducts/UpcommingProducts";
 
 const HomePage = () => {
   return (
     <div>
       <HomePageBanner />
       <OurProducts />
+      <UpcommingProducts />
+      <Footer />
     </div>
   );
 };
