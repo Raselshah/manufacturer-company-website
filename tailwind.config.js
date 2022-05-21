4335252113227f11c5a68bb582cc0e7a8e2e1940
Md Rasel Shah <mdraselshah56@gmail.com>
2022-05-21 22:35:36 +0600

    project initial

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#212F3C",
+          secondary: "#34495E",
+          accent: "#DC7633",
+          neutral: "#515A5A",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+};
