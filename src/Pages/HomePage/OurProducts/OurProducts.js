f2bb56341e13827f2800d6623b05b81c91d9f414
Md Rasel Shah <mdraselshah56@gmail.com>
2022-05-22 00:17:00 +0600

    product section add

--- src/Pages/HomePage/OurProducts/OurProducts.js
+++ src/Pages/HomePage/OurProducts/OurProducts.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import DisplayProducts from "./DisplayProducts";
 
 const OurProducts = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("fakeData.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
   return (
-      
     <div>
       <div className="text-center mt-16">
         <h3 className="font-bold text-accent text-lg">OUR PRODUCTS</h3>
@@ -12,6 +19,11 @@ const OurProducts = () => {
             Grow & Successful
           </span>
         </h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-16">
+          {products.map((product) => (
+            <DisplayProducts key={product._id} product={product} />
+          ))}
+        </div>
       </div>
     </div>
   );
